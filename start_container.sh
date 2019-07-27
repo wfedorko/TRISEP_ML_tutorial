534e35f3faeafa0d7892f98095687605ddfe909f
Wojciech Fedorko <wojtek.fedorko@gmail.com>
2019-07-26 17:11:27 -0700

    working on cosmetics of plotting training history

--- start_container.sh
+++ start_container.sh
@@ -1,1 +1,1 @@
-singularity exec --nv -B /fast_scratch -B /data /fast_scratch/triumfmlutils/containers/baseml_v0.3.1.sif /bin/bash
+singularity exec --nv -B /fast_scratch -B /data /fast_scratch/triumfmlutils/containers/baseml_v0.3.2.sif /bin/bash
--- utils/plot_utils.py
+++ utils/plot_utils.py
@@ -63,7 +63,9 @@ def tile(canvas, ul, pmts):
             
 
 
-def disp_learn_hist(train_log,val_log):
+def disp_learn_hist(location,losslim=None):
+    train_log=location+'/log_train.csv'
+    val_log=location+'/log_val.csv'
 
     train_log_csv = pd.read_csv(train_log)
     val_log_csv  = pd.read_csv(val_log)
@@ -71,7 +73,8 @@ def disp_learn_hist(train_log,val_log):
     fig, ax1 = plt.subplots(figsize=(12,8),facecolor='w')
     line11 = ax1.plot(train_log_csv.epoch, train_log_csv.loss, linewidth=2, label='Train loss', color='b', alpha=0.3)
     line12 = ax1.plot(val_log_csv.epoch, val_log_csv.loss, marker='o', markersize=3, linestyle='', label='Validation loss', color='blue')
-    
+    if losslim is not None:
+        ax1.set_ylim(0.,losslim)
     
     ax2 = ax1.twinx()
     line21 = ax2.plot(train_log_csv.epoch, train_log_csv.accuracy, linewidth=2, label='Train accuracy', color='r', alpha=0.3)
@@ -81,6 +84,7 @@ def disp_learn_hist(train_log,val_log):
     ax1.tick_params('x',colors='black',labelsize=18)
     ax1.set_ylabel('Loss', fontsize=24, fontweight='bold',color='b')
     ax1.tick_params('y',colors='b',labelsize=18)
+    
     
     ax2.set_ylabel('Accuracy', fontsize=24, fontweight='bold',color='r')
     ax2.tick_params('y',colors='r',labelsize=18)
@@ -96,7 +100,10 @@ def disp_learn_hist(train_log,val_log):
     plt.grid()
     plt.show()
     
-def disp_learn_hist_smoothed(train_log, val_log, window_train=400,window_val=40):
+def disp_learn_hist_smoothed(location, losslim=None, window_train=400,window_val=40):
+    train_log=location+'/log_train.csv'
+    val_log=location+'/log_val.csv'
+    
     train_log_csv = pd.read_csv(train_log)
     val_log_csv  = pd.read_csv(val_log)
 
@@ -108,26 +115,46 @@ def disp_learn_hist_smoothed(train_log, val_log, window_train=400,window_val=40)
     accuracy_val = moving_average(np.array(val_log_csv.accuracy),window_val)
     loss_val     = moving_average(np.array(val_log_csv.loss),window_val)
 
+    epoch_val_uns    = np.array(val_log_csv.epoch)
+    accuracy_val_uns = np.array(val_log_csv.accuracy)
+    loss_val_uns     = np.array(val_log_csv.loss)
+    saved_best=np.array(val_log_csv.saved_best)
+    stored_indices=np.where(saved_best>1.0e-3)
+    epoch_val_st=epoch_val_uns[stored_indices]
+    accuracy_val_st=accuracy_val_uns[stored_indices]
+    loss_val_st=loss_val_uns[stored_indices]
+
     fig, ax1 = plt.subplots(figsize=(12,8),facecolor='w')
     line11 = ax1.plot(epoch_train, loss_train, linewidth=2, label='Average training loss', color='b', alpha=0.3)
     line12 = ax1.plot(epoch_val, loss_val, label='Average validation loss', color='blue')
+    line13 = ax1.scatter(epoch_val_st, loss_val_st, label='BEST validation loss',
+                         facecolors='none', edgecolors='blue',marker='o')
     
     ax1.set_xlabel('Epoch',fontweight='bold',fontsize=24,color='black')
     ax1.tick_params('x',colors='black',labelsize=18)
     ax1.set_ylabel('Loss', fontsize=24, fontweight='bold',color='b')
     ax1.tick_params('y',colors='b',labelsize=18)
+    if losslim is not None:
+        ax1.set_ylim(0.,losslim)
     
     ax2 = ax1.twinx()
     line21 = ax2.plot(epoch_train, accuracy_train, linewidth=2, label='Average training accuracy', color='r', alpha=0.3)
     line22 = ax2.plot(epoch_val, accuracy_val, label='Average validation accuracy', color='red')
+    line23 = ax2.scatter(epoch_val_st, accuracy_val_st, label='BEST accuracy',
+                         facecolors='none', edgecolors='red',marker='o')
+    
     
     ax2.set_ylabel('Accuracy', fontsize=24, fontweight='bold',color='r')
     ax2.tick_params('y',colors='r',labelsize=18)
     ax2.set_ylim(0.,1.0)
     
     # added these four lines
-    lines  = line11 + line12 + line21 + line22
+    lines  = line11+ line12+ [line13]+ line21+ line22+ [line23]
+    #lines_sctr=[line13,line23]
+    #lines=lines_plt+lines_sctr
+
     labels = [l.get_label() for l in lines]
+    
     leg    = ax2.legend(lines, labels, fontsize=16, loc=5, numpoints=1)
     leg_frame = leg.get_frame()
     leg_frame.set_facecolor('white')
